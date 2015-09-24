efb7f925bcdb5ebbb1bae0f52cb7f0c2f743300c
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-25 01:25:19 +0900

    bundle gem itamae-plugin-recipe-docker

---
+++ lib/itamae/plugin/recipe/docker.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/docker/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Docker
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/docker/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Docker
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
